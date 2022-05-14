c4e62b55f24b1673560d8a320351f1477a2ceb2e
Thomas Honeyman <hello@thomashoneyman.com>
2022-05-14 13:35:32 -0400

    Update for PureScript 0.15

--- test/Snapshot.purs
+++ test/Snapshot.purs
@@ -2,7 +2,7 @@ module Test.Snapshot where
 
 import Prelude
 
-import Control.MonadZero (guard)
+import Control.Alternative (guard)
 import Control.Parallel (parTraverse)
 import Data.Array (mapMaybe)
 import Data.Array as Array
